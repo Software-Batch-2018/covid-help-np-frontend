c66d50389a7b71deb263590360610b5b068fcdfc
joerush18 <joras.aryal23@gmail.com>
2021-05-11 16:04:06 +0545

    Everythings done now search item to add the roue param

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
-import './App.css';
+import "./App.css";
+import Dashboard from "./components/Dashboard";
 import "./components/Header";
-import Header from './components/Header';
+import Header from "./components/Header";
+import ContentContainer from "./containers/ContentContainer";
 
 function App() {
   return (
     <div className="App">
-      <Header/>
-     <h1>This is start</h1>
+      <Header />
+      <Dashboard />
+      <ContentContainer />
     </div>
   );
 }
