ba679542fd1a45e65d01ec9717247460b80cbf8a
joerush18 <joras.aryal23@gmail.com>
2021-05-11 17:58:39 +0545

    some conditions are to be made

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,17 +1,26 @@
-import React from 'react'
-import { Container , Form , FormControl , Button, Jumbotron } from 'react-bootstrap'
+import React from "react";
+import {
+  Button,
+  Container,
+  Form,
+  FormControl,
+  Jumbotron,
+} from "react-bootstrap";
 import { SearchIcon } from "@heroicons/react/solid";
 const Dashboard = () => {
-    return (
-        <>   
-        <Jumbotron className="w-50 p-3">
-        <Form inline>   
-       <FormControl type="text" placeholder="Search for you city or Select from below" className="mr-4 w-100" />
-     {/* <Button variant="outline-info">Search</Button> */}
-       </Form>
+  return (
+    <>
+      <Jumbotron className="w-75 p-3">
+        <Form inline>
+          <FormControl
+            type="text"
+            placeholder="Search for you city or Select from below"
+            className="mr-4 w-100"
+          />
+          <SearchIcon className="h-1 w-1" />
+        </Form>
       </Jumbotron>
-        </>
-    )
-}
-
+    </>
+  );
+};
 export default Dashboard;
